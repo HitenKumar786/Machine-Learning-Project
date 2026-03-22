b8e3d9f858916f32b30e867dfc237c0f35457f77
HitenKumar786 <hitenkumarvendani@gmail.com>
2026-03-22 23:54:51 +0100

    Created Model Trainer file

---
+++ src/components/data_transformation.py
@@ -0,0 +1,90 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path: str = os.path.join('artifacts', 'preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns = ['writing_score', 'reading_score']
+
+            categorical_columns = [
+                'gender',
+                'race_ethnicity',
+                'parental_level_of_education',
+                'lunch',
+                'test_preparation_course'
+            ]
+
+            num_pipeline = Pipeline([
+                ('imputer', SimpleImputer(strategy='median')),
+                ('scaler', StandardScaler())
+            ])
+
+            cat_pipeline = Pipeline([
+                ('imputer', SimpleImputer(strategy='most_frequent')),
+                ('onehot', OneHotEncoder(handle_unknown='ignore')),
+                ('scaler', StandardScaler(with_mean=False))
+            ])
+
+            preprocessor = ColumnTransformer([
+                ('num', num_pipeline, numerical_columns),
+                ('cat', cat_pipeline, categorical_columns)
+            ])
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            preprocessor = self.get_data_transformer_object()
+
+            target_column = 'math_score'
+
+            # Split features & target
+            X_train = train_df.drop(columns=[target_column])
+            y_train = train_df[target_column]
+
+            X_test = test_df.drop(columns=[target_column])
+            y_test = test_df[target_column]
+
+            # Transform
+            X_train_transformed = preprocessor.fit_transform(X_train)
+            X_test_transformed = preprocessor.transform(X_test)
+
+            # Combine
+            train_arr = np.c_[X_train_transformed, y_train]
+            test_arr = np.c_[X_test_transformed, y_test]
+
+            # Save preprocessor
+            save_object(self.config.preprocessor_obj_file_path, preprocessor)
+
+            return train_arr, test_arr, self.config.preprocessor_obj_file_path
+
+        except Exception as e:
+            raise CustomException(e, sys)
---
+++ src/utils.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import dill
+
+from src.exception import CustomException
+from sklearn.metrics import r2_score
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            
+            # Train model
+            model.fit(X_train, y_train)
+
+            # Predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Scores
+            train_r2_square = r2_score(y_train, y_train_pred)
+            test_r2_square = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_r2_square
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
