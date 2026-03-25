2a076124e6faab8563420da19c99faa4bcecc649
HitenKumar786 <hitenkumarvendani@gmail.com>
2026-03-25 02:23:21 +0100

    Added Hyperparameter

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,8 +48,38 @@ class ModelTrainer:
                 'AdaBoost Regressor' : AdaBoostRegressor() 
 
             }
+            params={
+                'Random Forest':{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                'Decision Tree ':{
+                    'criterion': ['squared_error','friedman_mse','absolute_error','possion']
+                },
+                'Gradient Boosting':{
+                    'learning_rate':[0.1, 0.01, 0.05,0.001],
+                    'subsample':[0,6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [100, 200, 500]
+                },
+                'Linear Regression':{},
+                'K_Neighbors Classifier':{
+                    'n_neighbors': [5, 7, 9, 11]
+                },
+                'XGBRegressor':{
+                    'learning_rate':[0.1, 0.01, 0.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                'CatBoosting Regressor':{
+                    'iterations': [30, 50, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'depth': [6, 8, 10]
+                },
+                'AdaBoost Regressor':{
+                    'learning_rate':[0.1, 0.01,0.05, 0.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
 
-            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=model)
+            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=model,param=params)
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[
